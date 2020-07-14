35bc5027501ab6b54192cf4d7961e24ccb1d70ac
Hazel Wright <hazel.wright@zego.com>
2020-07-14 10:58:06 +0100

    wip

--- demo/blog/views.py
+++ demo/blog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post
+
+
+def create_new_post(request):
+    pass
+
+
+def update_post(request):
+    pass
+
+
+def delete_post(request):
+    pass
+
+
+def add_comment(request):
+    pass
+
+
+def remove_comment(request):
+    pass
